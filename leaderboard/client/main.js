703fc2fd204e53d9955456345ce2e0f144205163
ciwolsey <carl@wolsey.org>
2015-06-18 19:53:03 +0100

    Remove isClient checks

--- leaderboard/client/main.js
+++ leaderboard/client/main.js
@@ -4,60 +4,59 @@ Images = new FS.Collection("myImages", {
   stores: [new FS.Store.FileSystem("myImages", {path: "~/uploads"})]
 });
 
-if(Meteor.isClient){
-  Template.leaderboard.helpers({
-    'player': function(){
-      return PlayersList.find()
-    }
+Template.leaderboard.helpers({
+  'player': function(){
+    return PlayersList.find()
+  }
+});
+
+Template.addPlayerForm.events({
+  'submit form': function(e,t){
+
+   event.preventDefault();
+   var brandNameVar = event.target.brandName.value;
+   var modelNameVar = event.target.modelName.value;
+   var heightVar = event.target.height.value;
+   var widthVar = event.target.width.value;
+   var thicknessVar = event.target.thickness.value;
+   var boardTypeVar = event.target.boardType.value;
+   var tailTypeVar = event.target.tailType.value;
+   var finSetupVar = event.target.finSetup.value;
+   var finSystemVar = event.target.finSystem.value;
+   var locationVar = event.target.location.value;
+   var emailVar = event.target.email.value;
+   var phoneVar = event.target.phone.value;
+
+   console.log("brandName " + brandNameVar);
+   console.log(modelNameVar);
+   console.log(heightVar);
+   console.log(widthVar);
+   console.log(thicknessVar);
+   console.log(boardTypeVar);
+   console.log(tailTypeVar);
+   console.log(finSetupVar);
+   console.log(finSystemVar);
+   console.log(locationVar);
+   console.log(emailVar);
+   console.log(phoneVar);
+
+   PlayersList.insert({
+    brandName: brandNameVar,
+    modelName: modelNameVar,
+    height: heightVar,
+    width: widthVar,
+    thickness: thicknessVar,
+    boardType: boardTypeVar,
+    tailType: tailTypeVar,
+    finSetup: finSetupVar,
+    finSystem: finSystemVar,
+    location: locationVar,
+    email: emailVar,
+    phone:  phoneVar
+    
   });
-
-  Template.addPlayerForm.events({
-    'submit form': function(e,t){
-
-     event.preventDefault();
-     var brandNameVar = event.target.brandName.value;
-     var modelNameVar = event.target.modelName.value;
-     var heightVar = event.target.height.value;
-     var widthVar = event.target.width.value;
-     var thicknessVar = event.target.thickness.value;
-     var boardTypeVar = event.target.boardType.value;
-     var tailTypeVar = event.target.tailType.value;
-     var finSetupVar = event.target.finSetup.value;
-     var finSystemVar = event.target.finSystem.value;
-     var locationVar = event.target.location.value;
-     var emailVar = event.target.email.value;
-     var phoneVar = event.target.phone.value;
-
-     console.log("brandName " + brandNameVar);
-     console.log(modelNameVar);
-     console.log(heightVar);
-     console.log(widthVar);
-     console.log(thicknessVar);
-     console.log(boardTypeVar);
-     console.log(tailTypeVar);
-     console.log(finSetupVar);
-     console.log(finSystemVar);
-     console.log(locationVar);
-     console.log(emailVar);
-     console.log(phoneVar);
-
-     PlayersList.insert({
-      brandName: brandNameVar,
-      modelName: modelNameVar,
-      height: heightVar,
-      width: widthVar,
-      thickness: thicknessVar,
-      boardType: boardTypeVar,
-      tailType: tailTypeVar,
-      finSetup: finSetupVar,
-      finSystem: finSystemVar,
-      location: locationVar,
-      email: emailVar,
-      phone:  phoneVar
-      
-    });
-   }
- });
+ }
+});
 
 Template.dropzone.events({
   'change .myFileInput': function(event, template) {
@@ -70,4 +69,4 @@ Template.dropzone.events({
   }
 }); 
 
-} 
+
