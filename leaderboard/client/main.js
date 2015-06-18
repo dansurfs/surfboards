4457df091e961a32ae17ffbaea5a9156ef0f03f0
ciwolsey <carl@wolsey.org>
2015-06-18 20:55:39 +0100

    Fixed dropzone upload

--- leaderboard/client/main.js
+++ leaderboard/client/main.js
@@ -45,10 +45,35 @@ Template.dropzone.events({
     var files = event.target.files;
     for (var i = 0, ln = files.length; i < ln; i++) {
       Images.insert(files[i], function (err, fileObj) {
-        // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
+        console.log("Inserted file");
       });
     }
   }
 }); 
 
 
+Template.imageUpload.onRendered(function(){
+
+    var arrayOfImageIds = [];
+
+    Dropzone.autoDiscover = false;
+
+    // Adds file uploading and adds the imageID of the file uploaded
+    // to the arrayOfImageIds object.
+    var dropzone = new Dropzone("form#dropzone", {
+        accept: function(file, done){
+            Images.insert(file, function(err, file){
+                if(err){
+                  alert("Error");
+                } else {
+                  // gets the ID of the image that was uploaded
+                  var imageId = file._id;
+                  // do something with this image ID, like save it somewhere
+                  arrayOfImageIds.push(imageId);
+                  done();
+                };
+            });
+        }
+    });
+
+});
